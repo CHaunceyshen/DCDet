55522847e75fb20b9529084470af7602360995b2
CHaunceyshen <90432584+CHaunceyshen@users.noreply.github.com>
2024-06-16 11:44:24 +0800

    Update sods.py

--- mmrotate/models/necks/sods.py
+++ mmrotate/models/necks/sods.py
@@ -2,6 +2,10 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F
 from mmcv.cnn import xavier_init
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
 import math
 import warnings
 import e2cnn.nn as enn
@@ -221,7 +225,46 @@ class SE_ASPP(nn.Module):                       ##加入通道注意力机制
         # print('result:',result.shape)
         return result
 
+def HEAT(feature,str_dir):
 
+    img_name = "P0024__1024__524___2096"
+    img_path = '/media/yun/4t/work4t/scc/data/split-heat-map-images/test/images/' + img_name + '.png'
+    # heat 为某层的特征图，自己手动获取
+    # heat = feature.data.cpu().numpy()       # 将tensor格式的feature map转为numpy格式
+    # heat = np.squeeze(heat, 0)           # ０维为batch维度，由于是单张图片，所以batch=1，将这一维度删除
+    channel = [0,32,64,96,128,160,192,224,256]
+    channel = [0, 16, 32, 48, 64, 80, 96,112, 128,144, 160,176, 192,208, 224,240, 256]
+    list = []
+    for i in range(256):
+        flag = 32*i
+        if flag > 256:
+            break
+        list.append(flag)
+    # channel_end = 64
+    for i in range(len(list)-1):
+        heat = feature.data.cpu().numpy()  # 将tensor格式的feature map转为numpy格式
+        heat = np.squeeze(heat, 0)
+        channel_start = list[i]
+        channel_end = list[i+1]
+        heat = heat[channel_start:channel_end,:]       # 切片获取某几个通道的特征图
+        heatmap = np.maximum(heat, 0)        # heatmap与0比较
+        heatmap = np.mean(heatmap, axis=0)  # 多通道时，取均值
+        heatmap /= np.max(heatmap)          # 正则化到 [0,1] 区间，为后续转为uint8格式图做准备
+        # plt.matshow(heatmap)               # 可以通过 plt.matshow 显示热力图
+        # plt.show()
+
+        # 用cv2加载原始图像
+        img = cv2.imread(img_path)
+        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))  # 特征图的大小调整为与原始图像相同
+        heatmap = np.uint8(255 * heatmap)  # 将特征图转换为uint8格式
+        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)  # 将特征图转为伪彩色图
+        heat_img = cv2.addWeighted(img, 0.3, heatmap, 0.7, 0)     # 将伪彩色图与原始图片融合
+        #heat_img = heatmap * 0.5 + img 　　　　　　        　　　 # 也可以用这种方式融合
+        results_dir = "/media/yun/4t/work4t/scc/mmrotate-heat/work_dirs/heat-maps-ours-v100/"+img_name+str_dir
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+        results_path = results_dir + '_'  + str(channel_start) + "-" + str(channel_end)  + "_CAM" + ".png"
+        cv2.imwrite(results_path, heat_img)
 
 @ROTATED_NECKS.register_module()
 class SODS(BaseModule):
@@ -312,14 +355,14 @@ class SODS(BaseModule):
                 self.fpn_convs.append(extra_fpn_conv)
 
         # add scale sequence
-        self.conv3D_bn_act = nn.Sequential(
+        self.conv3D_bn_activation = nn.Sequential(
             nn.Conv3d(self.out_channels, self.out_channels, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
             nn.BatchNorm3d(self.out_channels),
             nn.LeakyReLU(0.01, inplace=True)
         )
         num_outs_3D = 4
-        self.avgpool = nn.AvgPool3d(kernel_size=(num_outs_3D, 1, 1), stride=(num_outs_3D, 1, 1), padding=(0, 0, 0))
-        self.conv_1_1 = nn.Conv2d(self.out_channels * 2, self.out_channels, 1, 1)
+        self.average_pooling = nn.AvgPool3d(kernel_size=(num_outs_3D, 1, 1), stride=(num_outs_3D, 1, 1), padding=(0, 0, 0))
+        self.refine_convolution = nn.Conv2d(self.out_channels * 2, self.out_channels, 1, 1)
 
     # default init_weights for conv(msra) and norm in ConvModule
     def init_weights(self):
@@ -373,24 +416,47 @@ class SODS(BaseModule):
                         outs.append(self.fpn_convs[i](outs[-1]))
 
         # multi-scale feature stream
-        last_p = outs[0]
-        list = []
-        size = outs[0].size()[-2:]
-        list.append(outs[0])
-        list.append(outs[1])
-        list.append(outs[2])
-        list.append(outs[3])
+        # 我们从第一个输出开始处理
+        final_output = outs[0]
+        processed_layers = []
+        # 获取最终输出的特征尺寸
+        feature_map_size = final_output.size()[-2:]
 
-        add_level_scale = [torch.unsqueeze(F.interpolate(out, size=size, mode='nearest'), dim=2) for out in
-                           list]
-        Pssf = torch.cat(add_level_scale, dim=2)
-        Pssf = self.conv3D_bn_act(Pssf)
-        Pssf = self.avgpool(Pssf)
-        Pssf = torch.squeeze(Pssf, dim=2)
-        # heat map
-        Pssf = torch.cat([Pssf, last_p], dim=1)
-        ps3 = self.conv_1_1(Pssf)
-        outs[0] = ps3
+        # 将outs中的每个输出添加到processed_layers列表zhong
+        processed_layers.append(final_output)
+        processed_layers.append(outs[1])
+        processed_layers.append(outs[2])
+        processed_layers.append(outs[3])
+        # heat_map
+        # HEAT(outs[0], 'dir_0')
+        # HEAT(outs[1], 'dir_1')
+        # HEAT(outs[2], 'dir_2')
+        # HEAT(outs[3], 'dir_3)
+
+        # 对每个输出进行尺寸调整并插值，增加time维度
+        adjusted_scales = [
+            torch.unsqueeze(
+                F.interpolate(layer, size=feature_map_size,model='nearest'), dim=2
+            ) for layer in processed_layers
+        ]
+
+        # 将调整的特征图沿着time维度拼接起来
+        combined_feature = torch.cat(adjusted_scales, dim=2)
+        # 处理特征图
+        processed_feature = self.conv3D_bn_activation(combined_feature)
+
+        pooled_features = self.average_pooling(processed_feature)
+        compressed_fearures = torch.squeeze(pooled_features, dim=2)
+
+        # 将处理后的特征图与P2最大尺度特征图进行合并
+        combined_feature_final = torch.cat([compressed_fearures, final_output], dim=1)
+        # 融合特征，降维
+        refined_output = self.refine_convolution(combined_feature_final)
+
+        # heat_map
+        #HEAT(refined_output, 'dir_sods')
+
+        outs[0] = refined_output
 
         return tuple(outs)
 
